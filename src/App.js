0e0de8422baca5694fa2b71a8715c3c4b18cbbca
Gabriellji <gabriellji@yahoo.com>
2020-11-15 02:11:15 +0300

    feat: update app fn to class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }  
 }
 
 export default App;

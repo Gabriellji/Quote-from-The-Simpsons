c476d0a582a6697771e2fb576b77c38efb66705d
Gabriellji <gabriellji@yahoo.com>
2020-11-15 02:25:13 +0300

    feat: getouote method using api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+
 import './App.css';
+import QuoteCard from './components/QuoteCard';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { quote: '' };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+    .then(response => response.data)
+    .then(data => {
+      console.log(data[0])
+      this.setState({ quote: data[0]})
+    })
+
   }
   render() {
     return (
       <div className="App">
+        <QuoteCard 
+        image={this.state.quote.image}
+        quote={this.state.quote.quote}
+        character={this.state.quote.character}
+        />
+        <button type="button" onClick={this.getQuote}>Get Quote</button>
       </div>
     );
   }  

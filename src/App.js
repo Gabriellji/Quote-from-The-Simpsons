b61b5e4c055f008b43583df77d990878f6838657
Gabriellji <gabriellji@yahoo.com>
2020-11-15 03:06:14 +0300

    refactor: components

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: {
-        quote: "Shut up, brain. I got friends now. I don't need you anymore.",
-        character: "Lisa Simpson",
-        image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
-        characterDirection: "Right"
-      }
+      quote: ''
     };
     this.getQuote = this.getQuote.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then(response => response.data)
+      .then(data => {
+        this.setState({ quote: data[0] })
+      });
   }
 
   getQuote() {
@@ -24,10 +27,10 @@ class App extends React.Component {
       .then(data => {
         this.setState({ quote: data[0] })
       });
+  }
 
-  };
-  
   render() {
+    console.log(this.state.quote)
     return (
       <div className="App">
         <QuoteCard
@@ -35,7 +38,7 @@ class App extends React.Component {
           quote={this.state.quote.quote}
           character={this.state.quote.character}
         />
-        <button  className="btn" type="button" onClick={this.getQuote}>Get Quote</button>
+        <button className="btn" type="button" onClick={this.getQuote}>Get Quote</button>
       </div>
     );
   }

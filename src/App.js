9df9d5a69dc2918ffee61a3282ea6ff0c2df20ba
Gabriellji <gabriellji@yahoo.com>
2020-11-15 23:41:59 +0300

    fix: style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.css';
+import LoadingSpinner from './components/LoadingSpinner';
 import QuoteCard from './components/QuoteCard';
 
 function App() {
 
-  const [quotes, setQuotes] = useState([]);
+  let [quotes, setQuotes, loading = [false]] = useState([]);
 
   useEffect(() => {
     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=3')
@@ -20,13 +21,16 @@ function App() {
     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=3')
       .then(res => {
         setQuotes(res.data);
+        loading.push(false);
       })
       .catch(error => console.log(error));
+      loading.shift();
   };
 
     return (
       <div className="App">
-        {quotes.map((el, i) => (
+        <div className="quote_wrap">
+        { !loading.includes(false) ? <LoadingSpinner /> : quotes.map((el, i) => (
           <QuoteCard
           key={i}
           image={el.image}
@@ -34,6 +38,7 @@ function App() {
           character={el.character}
         />
         ))}
+        </div>
         <button className="btn" type="button" onClick={getQuote}>Get Quote</button>
       </div>
     );

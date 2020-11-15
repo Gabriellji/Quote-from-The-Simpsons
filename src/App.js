806a0f5e62729f7f43f00a279388b93f60347a06
Gabriellji <gabriellji@yahoo.com>
2020-11-15 20:40:24 +0300

    feat: spiner and animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 
 import './App.css';
 import QuoteCard from './components/QuoteCard';
+import LoadingSpinner from './components/LoadingSpinner';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: ''
+      quote: '',
+      loading: false,
     };
     this.getQuote = this.getQuote.bind(this);
   }
@@ -23,21 +25,25 @@ class App extends React.Component {
 
   getQuote() {
     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
-      .then(response => response.data)
+      .then(response => {
+        this.setState({ loading: true })
+        return response.data
+      })
       .then(data => {
-        this.setState({ quote: data[0] })
+        this.setState({ quote: data[0], loading: false })
       });
   }
 
   render() {
-    console.log(this.state.quote)
+    const { loading } = this.state;
     return (
       <div className="App">
-        <QuoteCard
+        {loading ? <LoadingSpinner /> : <QuoteCard
           image={this.state.quote.image}
           quote={this.state.quote.quote}
           character={this.state.quote.character}
-        />
+        />}
+
         <button className="btn" type="button" onClick={this.getQuote}>Get Quote</button>
       </div>
     );
